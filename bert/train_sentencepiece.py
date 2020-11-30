2de90daf4ba92d7c1b46e2359ecaeaf32b9e04ac
7AM7 <gentlemanana1@gmail.com>
2020-11-30 21:51:23 +0200

    Remove tf.gfile

--- bert/train_sentencepiece.py
+++ bert/train_sentencepiece.py
@@ -3,7 +3,6 @@ import logging
 import argparse
 
 import sentencepiece as spm
-import tensorflow as tf
 
 logging.basicConfig(
     format="%(asctime)s : %(levelname)s : %(message)s",
@@ -138,7 +137,7 @@ def main():
     args = parser.parse_args()
 
     TrainSentencePiece(
-        input_file=tf.gfile.Glob(args.input_file),
+        input_file=args.input_file,
         save_dir=args.save_folder,
         model_prefix=args.model_prefix,
         vocab_size=args.vocab_size,

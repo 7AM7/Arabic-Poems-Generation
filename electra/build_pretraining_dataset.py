4bc03890a400bb76bb36116fcc5ff18f57b91d28
7AM7 <gentlemanana1@gmail.com>
2020-12-09 15:29:31 +0200

    Update build_pretraining_dataset.py script again2

--- electra/build_pretraining_dataset.py
+++ electra/build_pretraining_dataset.py
@@ -168,11 +168,7 @@ def write_examples(job_id, args):
     print("Job {}:".format(job_id), msg)
 
   log("Creating example writer")
-  fnames = []
-  for input_pattern in args.corpus_dir.split(','):
-    fnames.extend(tf.gfile.Glob(input_pattern))
-
-  fnames = sorted(fnames)
+  fnames = sorted(tf.io.gfile.listdir(args.corpus_dir))
   fnames = [f for (i, f) in enumerate(fnames)
             if i % args.num_processes == job_id]
   random.shuffle(fnames)

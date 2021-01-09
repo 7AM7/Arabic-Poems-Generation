740c6d175a3382f7ae49e4069839396681308821
ahmedkotb98 <ahmedflwkes10@gmail.com>
2021-01-09 16:21:58 +0200

    lm ith nsp

--- FineTuning/Finetuning_LM_NSP/fine-tuningLM.py
+++ FineTuning/Finetuning_LM_NSP/fine-tuningLM.py
@@ -1,20 +1,3 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HugginFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""BERT finetuning runner."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -32,6 +15,9 @@ from torch.utils.data.distributed import DistributedSampler
 from pytorch_pretrained_bert.tokenization import BertTokenizer
 from pytorch_pretrained_bert.modeling import BertForPreTraining
 from pytorch_pretrained_bert.optimization import BertAdam
+from transformers import XLNetTokenizer
+from transformers import BertForPreTraining
+import sentencepiece as spm
 
 from torch.utils.data import Dataset
 import random
@@ -46,6 +32,13 @@ def warmup_linear(x, warmup=0.002):
     if x < warmup:
         return x/warmup
     return 1.0 - x
+
+
+def accuracy(out, labels, total_test):
+    class_preds = out.data.cpu().numpy().argmax(axis=-1)
+    labels = labels.data.cpu().numpy()
+    return np.sum(class_preds == labels) / total_test
+
 
 
 class BERTDataset(Dataset):
@@ -414,15 +407,29 @@ def main():
                         type=str,
                         required=True,
                         help="The input train corpus.")
+    parser.add_argument("--test_file",
+                        default=None,
+                        type=str,
+                        required=True,
+                        help="The input test corpus.")
+                        
+    parser.add_argument("--tokenizer_model", default=None, type=str, required=True,
+                        help="tokenizer pre-trained model selected in the list: bert-base-uncased, "
+                             "bert-large-uncased, bert-base-cased, bert-base-multilingual, bert-base-chinese.")
+                             
     parser.add_argument("--bert_model", default=None, type=str, required=True,
                         help="Bert pre-trained model selected in the list: bert-base-uncased, "
                              "bert-large-uncased, bert-base-cased, bert-base-multilingual, bert-base-chinese.")
+                             
+    parser.add_argument("--config_file", default=None, type=str, required=True,
+                        help="Bert pre-trained model selected in the list: bert-base-uncased, "
+                             "bert-large-uncased, bert-base-cased, bert-base-multilingual, bert-base-chinese.")
+                             
     parser.add_argument("--output_dir",
                         default=None,
                         type=str,
                         required=True,
                         help="The output directory where the model checkpoints will be written.")
-
     ## Other parameters
     parser.add_argument("--max_seq_length",
                         default=128,
@@ -430,13 +437,12 @@ def main():
                         help="The maximum total input sequence length after WordPiece tokenization. \n"
                              "Sequences longer than this will be truncated, and sequences shorter \n"
                              "than this will be padded.")
-    parser.add_argument("--do_train",
-                        action='store_true',
-                        help="Whether to run training.")
+                             
     parser.add_argument("--train_batch_size",
                         default=32,
                         type=int,
                         help="Total batch size for training.")
+                        
     parser.add_argument("--eval_batch_size",
                         default=8,
                         type=int,
@@ -445,39 +451,49 @@ def main():
                         default=3e-5,
                         type=float,
                         help="The initial learning rate for Adam.")
+                        
     parser.add_argument("--num_train_epochs",
                         default=3.0,
                         type=float,
                         help="Total number of training epochs to perform.")
+                        
     parser.add_argument("--warmup_proportion",
                         default=0.1,
                         type=float,
                         help="Proportion of training to perform linear learning rate warmup for. "
                              "E.g., 0.1 = 10%% of training.")
+                             
     parser.add_argument("--no_cuda",
                         action='store_true',
                         help="Whether not to use CUDA when available")
+                        
     parser.add_argument("--on_memory",
                         action='store_true',
                         help="Whether to load train samples into memory or use disk")
+                        
     parser.add_argument("--do_lower_case",
                         action='store_true',
                         help="Whether to lower case the input text. True for uncased models, False for cased models.")
+                        
     parser.add_argument("--local_rank",
                         type=int,
                         default=-1,
                         help="local_rank for distributed training on gpus")
+                        
     parser.add_argument('--seed',
                         type=int,
                         default=42,
                         help="random seed for initialization")
+                        
     parser.add_argument('--gradient_accumulation_steps',
                         type=int,
                         default=1,
                         help="Number of updates steps to accumualte before performing a backward/update pass.")
+                        
     parser.add_argument('--fp16',
                         action='store_true',
                         help="Whether to use 16-bit float precision instead of 32-bit")
+                        
     parser.add_argument('--loss_scale',
                         type = float, default = 0,
                         help = "Loss scaling to improve fp16 numeric stability. Only used when fp16 set to True.\n"
@@ -517,19 +533,32 @@ def main():
         raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
     os.makedirs(args.output_dir, exist_ok=True)
 
-    tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=args.do_lower_case)
+    #tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=args.do_lower_case)
+    tokenizer = XLNetTokenizer.from_pretrained(args.tokenizer_model)
+    
 
     #train_examples = None
     num_train_steps = None
-    if args.do_train:
-        print("Loading Train Dataset", args.train_file)
-        train_dataset = BERTDataset(args.train_file, tokenizer, seq_len=args.max_seq_length,
+    
+    print("Loading Train Dataset", args.train_file)
+    train_dataset = BERTDataset(args.train_file, tokenizer, seq_len=args.max_seq_length,
                                     corpus_lines=None, on_memory=args.on_memory)
-        num_train_steps = int(
-            len(train_dataset) / args.train_batch_size / args.gradient_accumulation_steps * args.num_train_epochs)
+                                    
+                                    
+    print("Loading Train Dataset", args.test_file)                                
+    test_dataset =BERTDataset(args.test_file, tokenizer, seq_len=args.max_seq_length,
+                                    corpus_lines=None, on_memory=args.on_memory)
+                                    
+    num_train_steps = int(
+         len(train_dataset) / args.train_batch_size / args.gradient_accumulation_steps * args.num_train_epochs)
 
     # Prepare model
-    model = BertForPreTraining.from_pretrained(args.bert_model)
+    model = BertForPreTraining.from_pretrained(
+    args.bert_model,
+    config=args.config_file,
+    output_attentions = False, 
+    output_hidden_states = False, )
+    
     if args.fp16:
         model.half()
     model.to(device)
@@ -572,54 +601,89 @@ def main():
                              t_total=num_train_steps)
 
     global_step = 0
-    if args.do_train:
-        logger.info("***** Running training *****")
-        logger.info("  Num examples = %d", len(train_dataset))
-        logger.info("  Batch size = %d", args.train_batch_size)
-        logger.info("  Num steps = %d", num_train_steps)
+    
+    logger.info("***** Running training *****")
+    logger.info("  Num examples = %d", len(train_dataset))
+    logger.info("  Batch size = %d", args.train_batch_size)
+    logger.info("  Num steps = %d", num_train_steps)
 
-        if args.local_rank == -1:
-            train_sampler = RandomSampler(train_dataset)
+    if args.local_rank == -1:
+        train_sampler = SequentialSampler(train_dataset)
+        eval_sampler = SequentialSampler(eval_dataset)
+        
+    else:
+        #TODO: check if this works with current data generator from disk that relies on file.__next__
+        # (it doesn't return item back by index)
+        train_sampler = DistributedSampler(train_dataset)
+        test_sampler  = DistributedSampler(test_dataset)
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
+    train_dataloader = DataLoader(test_dataset, sampler=test_sampler, batch_size=args.train_batch_size)
+    
+
+    model.train()
+    tr_loss = 0
+    acc=0
+    train_loss=0.0
+    nb_tr_examples, nb_tr_steps = 0, 0
+    for _ in trange(int(args.num_train_epochs), desc="Epoch"):
+      for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
+        batch = tuple(t.to(device) for t in batch)
+        input_ids, input_mask, segment_ids, lm_label_ids, is_next = batch
+        outputs = model(input_ids, segment_ids, input_mask, lm_label_ids, is_next)
+        loss = outputs.loss
+        if n_gpu > 1:
+            loss = loss.mean() # mean() to average on multi-gpu.
+        if args.gradient_accumulation_steps > 1:
+            loss = loss / args.gradient_accumulation_steps
+        if args.fp16:
+            optimizer.backward(outputs.loss)
         else:
-            #TODO: check if this works with current data generator from disk that relies on file.__next__
-            # (it doesn't return item back by index)
-            train_sampler = DistributedSampler(train_dataset)
-        train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
+            loss.backward()
+        tr_loss += loss.item()
+        nb_tr_examples += input_ids.size(0)
+        nb_tr_steps += 1
+        if (step + 1) % args.gradient_accumulation_steps == 0:
+            # modify learning rate with special warm up BERT uses
+            lr_this_step = args.learning_rate * warmup_linear(global_step/num_train_steps, args.warmup_proportion)
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr_this_step
+            optimizer.step()
+            optimizer.zero_grad()
+            global_step += 1
 
-        model.train()
-        for _ in trange(int(args.num_train_epochs), desc="Epoch"):
-            tr_loss = 0
-            nb_tr_examples, nb_tr_steps = 0, 0
-            for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
-                batch = tuple(t.to(device) for t in batch)
-                input_ids, input_mask, segment_ids, lm_label_ids, is_next = batch
-                loss = model(input_ids, segment_ids, input_mask, lm_label_ids, is_next)
-                if n_gpu > 1:
-                    loss = loss.mean() # mean() to average on multi-gpu.
-                if args.gradient_accumulation_steps > 1:
-                    loss = loss / args.gradient_accumulation_steps
-                if args.fp16:
-                    optimizer.backward(loss)
-                else:
-                    loss.backward()
-                tr_loss += loss.item()
-                nb_tr_examples += input_ids.size(0)
-                nb_tr_steps += 1
-                if (step + 1) % args.gradient_accumulation_steps == 0:
-                    # modify learning rate with special warm up BERT uses
-                    lr_this_step = args.learning_rate * warmup_linear(global_step/num_train_steps, args.warmup_proportion)
-                    for param_group in optimizer.param_groups:
-                        param_group['lr'] = lr_this_step
-                    optimizer.step()
-                    optimizer.zero_grad()
-                    global_step += 1
+    train_loss = tr_loss / nb_tr_steps
+    perplexity = torch.exp(torch.tensor(train_loss)).item()
 
-        # Save a trained model
-        logger.info("** ** * Saving fine - tuned model ** ** * ")
-        model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
-        output_model_file = os.path.join(args.output_dir, "pytorch_model.bin")
-        if args.do_train:
-            torch.save(model_to_save.state_dict(), output_model_file)
+    print("Training loss {} ".format("{:.3f}".format(train_loss)))
+    print("Training perplexity {}".format("{:.3f}".format(perplexity)))
+
+    logger.info("***** Running evaluation *****")
+    logger.info("  Num examples = %d", len(eval_dataset))
+    logger.info("  Batch size = %d", batch_size)
+    eval_loss = 0.0
+    acc = 0
+    nb_eval_steps = 0
+    for batch in tqdm_notebook(test_dataloader, desc='Evaluating'):
+        batch = tuple(t.to(device) for t in batch)
+        input_ids, input_mask, segment_ids, lm_label_ids, is_next = batch
+        with torch.no_grad():
+            outputs = model(input_ids, segment_ids, input_mask, lm_label_ids, is_next)
+            loss = outputs.loss
+            eval_loss += loss.mean().item()
+        nb_eval_steps += 1
+
+    eval_loss = eval_loss / nb_eval_steps
+    perplexity = torch.exp(torch.tensor(eval_loss)).item()
+
+    print("Evalution loss {} ".format("{:.3f}".format(eval_loss)))
+    print("Evalution perplexity {}".format("{:.3f}".format(perplexity)))
+
+    # Save a trained model
+    logger.info("** ** * Saving fine - tuned model ** ** * ")
+    model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
+    output_model_file = os.path.join(args.output_dir, "pytorch_model.bin")
+    if args.do_train:
+        torch.save(model_to_save.state_dict(), output_model_file)
 
 
 def _truncate_seq_pair(tokens_a, tokens_b, max_length):
@@ -638,11 +702,11 @@ def _truncate_seq_pair(tokens_a, tokens_b, max_length):
         else:
             tokens_b.pop()
 
-
+'''
 def accuracy(out, labels):
     outputs = np.argmax(out, axis=1)
     return np.sum(outputs == labels)
-
+'''
 
 if __name__ == "__main__":
     main()
